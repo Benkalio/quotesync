b4f401159e93616e93726ad03dfc119d133d4bb7
Benkalio <officialtennysonbk@gmail.com>
2023-01-19 14:30:35 +0100

    Practicing react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './style.css';
+import { useState, useEffect } from 'react';
+
+function requestRandomQuotes(quotes) {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+
+  // const zen_url = 'https://zenquotes.io/api/quotes/';
+  const fit_url = "https://type.fit/api/quotes";
+
+  useEffect(() => { 
+    fetch(fit_url)
+      .then((res) => res.json())
+      .then((json) => {
+        setQuotes(json);
+        setQuote(json[0]);
+      });
+  }, []);
+
+  function requestNewQuote() {
+    setQuote(requestRandomQuotes(quotes));
+  }
+
   return (
-    <div className="App">
-     
-    </div>
+    <main>
+      <h1>Sync Random Quotes</h1>
+      <section>
+        <button onClick={requestNewQuote}>New Quote</button>
+        <h3>
+          <span>"</span>
+          {quote?.text}
+        </h3>
+        <i>~ {quote?.author}</i>
+      </section>
+    </main>
   );
 }
 
